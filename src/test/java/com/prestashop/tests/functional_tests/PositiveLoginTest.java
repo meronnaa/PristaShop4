c739975cd52156f721541016b01f14314de66e08
meronnaa <46093321+meronnaa@users.noreply.github.com>
2019-01-21 21:26:12 -0500

    Update PositiveLoginTest.java

--- src/test/java/com/prestashop/tests/functional_tests/PositiveLoginTest.java
+++ src/test/java/com/prestashop/tests/functional_tests/PositiveLoginTest.java
@@ -75,8 +75,7 @@ public class PositiveLoginTest extends TestBase {
         Assert.assertNotEquals(driver.getTitle(),"Web Orders Login");
         //9.Verify the current url equals the string saved in step 4
         Assert.assertNotEquals(currentUrl,driver.getCurrentUrl());
-        //7.Click on Login button
-        homepage.login1.click();
+       
     }
    
        
